7f507bf06912bbde3ba194eb6c82948894ca8558
yuriiprots <uraprots7849@gmail.com>
2024-03-12 18:51:01 +0200

    add a click event to the `operationsTabs` and implement the `showSlide` function

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const headerTop = document.querySelector(".header__top");
 const navLinks = document.querySelectorAll(".nav__link");
+const operationsTabs = document.querySelectorAll(".operations__tab");
+const operationsContentDivs = document.querySelectorAll(".operations__content");
+
+const slides = document.querySelectorAll(".slide");
+const slideFirst = document.querySelector(".slide--1");
+const slideSecond = document.querySelector(".slide--2");
+const slideThird = document.querySelector(".slide--3");
+
+const dots = document.querySelectorAll(".dots__dot");
+
+const sliderBtnLeft = document.querySelector(".slider__btn--left");
+const sliderBtnRight = document.querySelector(".slider__btn--right");
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > 550) {
@@ -40,7 +52,6 @@ navLinks.forEach((link) => {
   link.addEventListener("click", smoothScroll);
 });
 
-
 /*Lazy Loading Images*/
 
 let lazyImages = document.querySelectorAll(".features__img");
@@ -70,3 +81,74 @@ let imageObserver = new IntersectionObserver(
 lazyImages.forEach((image) => {
   imageObserver.observe(image);
 });
+
+/* ---------- */
+
+const deactiveAllTabs = () => {
+  operationsTabs.forEach((operationsTabs) =>
+    operationsTabs.classList.remove("operations__tab--active")
+  );
+  operationsContentDivs.forEach((operationsContentDiv) =>
+    operationsContentDiv.classList.remove("operations__content--active")
+  );
+};
+
+const addActiveTab = (clickedTab) => {
+  clickedTab.classList.add("operations__tab--active");
+
+  const contentIndex = parseInt(clickedTab.classList[2].split("--")[1]) - 1;
+  operationsContentDivs[contentIndex].classList.add(
+    "operations__content--active"
+  );
+};
+const handleOperationsTabsClick = (clickedTab) => {
+  deactiveAllTabs();
+  addActiveTab(clickedTab);
+};
+
+operationsTabs.forEach((operationsTab) => {
+  operationsTab.addEventListener("click", () =>
+    handleOperationsTabsClick(operationsTab)
+  );
+});
+
+/* ---------- */
+
+let slideIndex = 0;
+const totalSlides = document.querySelectorAll(".slide").length;
+
+const showSlide = (index) => {
+  if (index >= totalSlides || index === 0) {
+    slideIndex = 0;
+    slides[slideIndex].style.transform = `translateX(${0}%)`;
+    slides[slideIndex + 1].style.transform = `translateX(${100}%)`;
+    slides[slideIndex + 2].style.transform = `translateX(${200}%)`;
+  } else if (index < 0 || index === 2) {
+    slideIndex = totalSlides - 1;
+    slides[slideIndex - 2].style.transform = `translateX(-${200}%)`;
+    slides[slideIndex - 1].style.transform = `translateX(-${100}%)`;
+    slides[slideIndex].style.transform = `translateX(${0}%)`;
+  } else if (index === 1) {
+    slides[slideIndex - 1].style.transform = `translateX(-${100}%)`;
+    slides[slideIndex].style.transform = `translateX(${0}%)`;
+    slides[slideIndex + 1].style.transform = `translateX(${100}%)`;
+  }
+
+  dots.forEach((dot) => {
+    dot.classList.remove("dots__dot--active");
+  });
+  dots[slideIndex].classList.add("dots__dot--active");
+};
+
+const prevSlide = () => {
+  slideIndex--;
+  showSlide(slideIndex);
+};
+
+const nextSlide = () => {
+  slideIndex++;
+  showSlide(slideIndex);
+};
+
+sliderBtnLeft.addEventListener("click", prevSlide);
+sliderBtnRight.addEventListener("click", nextSlide);
